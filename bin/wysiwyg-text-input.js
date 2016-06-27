f250cdb0173e311c5d00f5d4860ce7ec9696d884
Kouji Nomura <kon_nya@hotmail.com>
2016-06-27 15:13:57 +0900

    support ie11

--- bin/wysiwyg-text-input.js
+++ bin/wysiwyg-text-input.js
@@ -50,6 +50,8 @@
 	    value: true
 	});
 
+	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -58,7 +60,7 @@
 	    function WysiwygTextInput(options) {
 	        _classCallCheck(this, WysiwygTextInput);
 
-	        this.options = Object.assign({}, {
+	        this.options = _extends({}, {
 	            target: ".value-cell",
 	            input_field_tag_id: "wysiwyg-text-input",
 	            input_field_wrapper_id: "wrapper-wysiwyg-text-input",
